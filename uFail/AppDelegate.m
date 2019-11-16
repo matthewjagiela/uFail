813207fe6b86f4cb9d7a39660d6f58b3c196341b
Matthew Jagiela <matthewjagiela@icloud.com>
2019-11-16 00:03:11 -0500

    Download on launch

--- uFail/AppDelegate.m
+++ uFail/AppDelegate.m
@@ -17,6 +17,7 @@
 {
     // Override point for customization after application launch.
     [[GADMobileAds sharedInstance] startWithCompletionHandler:nil];
+    [[AppHandler alloc]init];
     return YES;
 }
 							
