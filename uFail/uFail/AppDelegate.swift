199500042c542098c49a8cf93c75d4a4de84ca59
Matthew Jagiela <matthewjagiela@icloud.com>
2019-12-26 16:34:26 -0500

    New Project Setup

---
+++ uFail/uFail/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  uFail
+//
+//  Created by Matthew Jagiela on 12/26/19.
+//  Copyright © 2019 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ uFail/uFail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uFail
+//
+//  Created by Matthew Jagiela on 12/26/19.
+//  Copyright © 2019 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
