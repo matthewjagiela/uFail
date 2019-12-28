029156b6f22f7cbee03427f7f54fc734a9d43eb8
Matthew Jagiela <matthewjagiela@icloud.com>
2019-12-27 22:14:53 -0500

    Foundations

---
+++ uFail/Classes/Handlers/DataHandler.swift
@@ -0,0 +1,18 @@
+//
+//  DataHandler.swift
+//  uFail
+//
+//  Created by Matthew Jagiela on 12/27/19.
+//  Copyright © 2019 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+class DataHandler: NSObject {
+    //MARK: - Storing
+    //MARK: - Retriving
+}
+
+class iCloudHandler: NSObject {
+    
+}
