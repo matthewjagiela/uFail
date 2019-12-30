a72a824865e418a7bc90453f616f579594746568
Matthew Jagiela <matthewjagiela@icloud.com>
2019-12-29 23:36:25 -0500

    Playsound implementation with error.

--- uFail/Classes/Handlers/SoundHandler.swift
+++ uFail/Classes/Handlers/SoundHandler.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundHandler: NSObject {
     var sound: Sound = .uFail
-    let data = DataHandler()
     public enum Sound {
         case airhorn
         case epicFail
         case fail
+        case failure
         case noob
         case pwned
         case santa
@@ -24,6 +25,43 @@ class SoundHandler: NSObject {
     }
     func selectSound(_ sound: Sound) {
         self.sound = sound
+    }
+    func playSound() {
+        var player: AVAudioPlayer?
+        var path: String = ""
+        var audioURL: URL?
+        var fileName: String = ""
+        switch sound {
+        case .airhorn:
+            fileName = "AIRHORN.mp3"
+        case .epicFail:
+            fileName = "EPIC FAIL.mp3"
+        case .fail:
+            fileName = "FAIL.mp3"
+        case .failure:
+            fileName = "failure.mp3"
+        case .noob:
+            fileName = "Noob!.mp3"
+        case .pwned:
+            fileName = "PWNED.mp3"
+        case .santa:
+            fileName = "santa.mp3"
+        case .soManyFriends:
+            fileName = "So Many Friends.mp3"
+        case .win:
+            fileName = "WIN.mp3"
+        default:
+            fileName = "You Fail.mp3"
+        }
+        path = Bundle.main.path(forResource: "\(fileName)", ofType: nil) ?? ""
+        audioURL = URL(fileURLWithPath: path)
+        do {
+            player = try AVAudioPlayer(contentsOf: audioURL ?? URL(fileURLWithPath: path))
+            player?.play()
+        } catch {
+            print("ERROR!!!! SOUND! = \(error.localizedDescription)")
+        }
+        
     }
     
 }
