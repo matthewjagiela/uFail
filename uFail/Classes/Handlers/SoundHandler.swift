587edd2eaa22d5ae61d9b43fd6997765b8d180dc
Matthew Jagiela <matthewjagiela@icloud.com>
2019-12-29 22:14:11 -0500

    Sound Handler + Sounds

---
+++ uFail/Classes/Handlers/SoundHandler.swift
@@ -0,0 +1,13 @@
+//
+//  SoundHandler.swift
+//  uFail
+//
+//  Created by Matthew Jagiela on 12/29/19.
+//  Copyright © 2019 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+class SoundHandler: NSObject {
+    
+}
