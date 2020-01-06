e76096c18145d33b7e4cee5a49d8f7ac26c53da4
Matthew Jagiela <matthewjagiela@icloud.com>
2020-01-05 21:27:04 -0500

    Init

---
+++ uFail/Classes/ViewControllers/SelectorViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SelectorViewController.swift
+//  uFail
+//
+//  Created by Matthew Jagiela on 1/5/20.
+//  Copyright © 2020 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+import SideMenuSwift
+class SelectorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SideMenuControllerDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
