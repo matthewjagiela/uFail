6d71c39fa87608aec4eef13b0aa542e2318a2d95
Matthew Jagiela <matthewjagiela@icloud.com>
2020-01-05 21:29:24 -0500

    WhitespaceFIX

--- uFail/Classes/ViewControllers/SelectorViewController.swift
+++ uFail/Classes/ViewControllers/SelectorViewController.swift
@@ -16,7 +16,6 @@ class SelectorViewController: UIViewController, UITableViewDelegate, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,16 +23,5 @@ class SelectorViewController: UIViewController, UITableViewDelegate, UITableView
         // Do any additional setup after loading the view.
         
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
