890f9658397f128478647b6d9e853fc42435317c
Matthew Jagiela <matthewjagiela@icloud.com>
2020-01-16 22:18:26 -0500

    Safari Controllers working

--- uFail/Classes/ViewControllers/InfoViewController.swift
+++ uFail/Classes/ViewControllers/InfoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class InfoViewController: UIViewController {
 
@@ -21,6 +22,9 @@ class InfoViewController: UIViewController {
     @IBOutlet weak var uAppsNewsLabel: UILabel!
     @IBOutlet weak var copyrightLabel: UILabel!
     @IBOutlet weak var backgroundImage: UIImageView!
+    // MARK: - Class Variables
+    let info = AppHandler()
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         uAppsNewsLabel.adjustsFontSizeToFitWidth = true
@@ -28,7 +32,12 @@ class InfoViewController: UIViewController {
         newestVersionAvailable.adjustsFontSizeToFitWidth = true
         // Do any additional setup after loading the view.
     }
+    // MARK: - Actions
     @IBAction func supportTapped(_ sender: Any) {
+        if let url = URL(string: "https://uappsios.com/ufail-support") {
+            let safari = SFSafariViewController(url: url)
+            present(safari, animated: true, completion: nil)
+        }
     }
     @IBAction func homeTapped(_ sender: Any) {
     }
@@ -37,6 +46,10 @@ class InfoViewController: UIViewController {
     @IBAction func downloaduTime(_ sender: Any) {
     }
     @IBAction func viewuFailPrivacyPolicy(_ sender: Any) {
+        if let url = URL(string: "https://uappsios.com/ufail-privacy-policy") {
+            let safari = SFSafariViewController(url: url)
+            present(safari, animated: true, completion: nil)
+        }
     }
     /*
     // MARK: - Navigation
