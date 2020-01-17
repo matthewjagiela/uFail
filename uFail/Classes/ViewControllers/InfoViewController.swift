69cf0627a19f7b595d5895669b2c2840345d5005
Matthew Jagiela <matthewjagiela@icloud.com>
2020-01-16 20:54:28 -0500

    uAppsNewsLabel placement and scaling

--- uFail/Classes/ViewControllers/InfoViewController.swift
+++ uFail/Classes/ViewControllers/InfoViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    @IBOutlet weak var uAppsNewsLabel: UILabel!
     @IBOutlet weak var copyrightLabel: UILabel!
     @IBOutlet weak var backgroundImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        uAppsNewsLabel.adjustsFontSizeToFitWidth = true
         // Do any additional setup after loading the view.
     }
     /*
