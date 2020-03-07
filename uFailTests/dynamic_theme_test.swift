b39db2f49a14788fa91643b92ae718de36b67e13
Matthew Jagiela <matthewjagiela@icloud.com>
2020-03-07 17:45:53 -0500

    Simple test.

---
+++ uFailTests/dynamic_theme_test.swift
@@ -0,0 +1,13 @@
+//
+//  dynamic_theme_test.swift
+//  uFailTests
+//
+//  Created by Matthew Jagiela on 3/6/20.
+//  Copyright © 2020 Matthew Jagiela. All rights reserved.
+//
+
+import UIKit
+
+class dynamic_theme_test: XCTestCase {
+
+}
